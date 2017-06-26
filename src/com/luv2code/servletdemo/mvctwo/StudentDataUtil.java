9131b843ad4cf0c06cbe891acb6022d1b8fb023a
ThisIsTyDell <milleb94@gmail.com>
2017-06-26 12:09:06 -0500

    create an empty list, add sample data, return the list

--- src/com/luv2code/servletdemo/mvctwo/StudentDataUtil.java
+++ src/com/luv2code/servletdemo/mvctwo/StudentDataUtil.java
@@ -1,5 +1,23 @@
 package com.luv2code.servletdemo.mvctwo;
 
-public class StudentDataUtil {
+import java.util.ArrayList;
+import java.util.List;
 
+public class StudentDataUtil {
+	
+	public static List<Student> getStudents() {
+		
+		// create an empty list
+		List<Student> students = new ArrayList<>();
+		
+		// add sample data
+		students.add(new Student("Michael", "Jackson", "mj@email.com"));
+		students.add(new Student("Steve", "Jobs", "steviej@email.com"));
+		students.add(new Student("Elon", "Musk", "teslaboyz@email.com"));
+		
+		// return the list
+		return students;
+		
+	}
+	
 }
