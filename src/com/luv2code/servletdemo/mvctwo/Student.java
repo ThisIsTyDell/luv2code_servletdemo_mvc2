f4f929265c14e791ff11d1016df3be01d5c368ea
ThisIsTyDell <milleb94@gmail.com>
2017-06-26 12:00:22 -0500

    add constructors

--- src/com/luv2code/servletdemo/mvctwo/Student.java
+++ src/com/luv2code/servletdemo/mvctwo/Student.java
@@ -4,4 +4,13 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	private String email;
+	
+	public Student(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
+	
 }
